e1f1b624e9c907e37d7ccfd3a85731024037e9b6
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-05-11 19:18:35 +0300

    502 Login.

---
+++ src/test/java/FetureFiles/Login.feature
@@ -0,0 +1,6 @@
+Feature: Login functionality
+
+  Scenario: Login with valid username and password
+
+    Given Navigate to ParaBank
+    When Enter username and password and click login button
--- src/test/java/StepDefinition/Login.java
+++ src/test/java/StepDefinition/Login.java
@@ -1,4 +1,29 @@
 package StepDefinition;
 
-public class Login {
-}
+import Pages.NewAccountPage;
+import Pages.ParentPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+
+import static Utility.GWD_.getDriver;
+
+
+public class Login extends ParentPage {
+    NewAccountPage dc = new NewAccountPage();
+
+    @Given("Navigate to ParaBank")
+    public void navigate_to_campus() {
+        getDriver().get("https://parabank.parasoft.com/");
+    }
+
+    @When("Enter username and password and click login button")
+    public void enter_username_and_password_and_click_login_button() {
+        dc.username.sendKeys("Team02");
+        dc.password.sendKeys("92kteHpxHcnq@");
+        dc.loginButton.click();
+
+    }
+
+        }
+
